a75bf6bd26e9f40c1d2a265bb06721e6fde05460
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-31 20:41:59 -0400

    updated overview card border

--- src/components/OverviewCard.tsx
+++ src/components/OverviewCard.tsx
@@ -10,7 +10,7 @@ const OverviewCard = ({ flashcard }: OverviewCardProps) => {
     const { id, topic } = flashcard;
 
     return (
-        <div className="w-full h-full flex flex-col gap-2 p-6 border border-gray-100 border-solid rounded-2xl shadow-lg">
+        <div className="w-full h-full flex flex-col gap-2 p-6 border border-gray-200 border-solid rounded-2xl shadow-lg">
             <h3 className="text-2xl font-bold">{topic}</h3>
             <div>{topic}</div>
             <Link to={`/flashcard/${id}`}>
