df74cdf42a621d49bc535f4feb40aea565a4b7a1
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-31 20:35:34 -0400

    added messge for empty list

--- src/components/OverviewCardList.tsx
+++ src/components/OverviewCardList.tsx
@@ -1,13 +1,19 @@
 import OverviewCard from './OverviewCard';
 import { FlashCard } from '../types/flashcard';
+import tw from 'tailwind-styled-components';
 
 interface OverviewCardListProps {
     flashcards: FlashCard[];
 }
 
+const EmptyMsg = tw.div`
+    text-center text-gray-500
+`;
+
 const OverviewCardList = ({ flashcards }: OverviewCardListProps) => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {flashcards.length === 0 && <EmptyMsg>No flashcards yet. Generate your first set!</EmptyMsg>}
             {flashcards.map((flashcard) => (
                 <OverviewCard key={flashcard.id} flashcard={flashcard} />
             ))}
